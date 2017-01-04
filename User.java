f30c17aa30ed500a994b503d51b7ef06687c615a
lihongtu <253954665@qq.com>
2017-01-04 21:12:01 +0800

    add java bean User

---
+++ User.java
@@ -0,0 +1,32 @@
+public class User{
+      private String name;
+
+      private String gender;
+
+      private int age;
+
+      public void setName(String name){
+           this.name = name;
+      }
+
+      public String getName(){
+	   return this.name;
+      }
+
+      public void setAge(int age){
+           this.age = age;
+      }
+
+      public int getAge(){
+           return this.age;
+      }
+
+      public void setGenger(String gender){
+           this.gender = gender;
+      }
+
+      public String getGender(){
+           return this.gender;
+      }
+
+}
